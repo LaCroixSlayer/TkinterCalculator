37a581a9f2c51bab5735369bec8e39c12ab2aad8
LaCroixSlayer <lacroixslayer@gmail.com>
2020-06-17 23:16:10 -0500

    Added the add & clear functions.

--- calculator.py
+++ calculator.py
@@ -9,25 +9,47 @@ root.title("Calculator")
 userEntry = Entry(root, width=35, borderwidth=5)
 userEntry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
+#Global Variables
 
-def button_add():
+
+
+def buttonClick(number):
+	currentNum = userEntry.get()
+	userEntry.delete(0, END)
+	#Insert lambda number into the Entry widget
+	#We want to concatenate two values and NOT add so we use str().
+	userEntry.insert(0, str(currentNum) + str(number))
 	return
 
-#Add Button widgets
-btn1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-btn2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-btn3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-btn4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-btn5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-btn6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-btn7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-btn8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-btn9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-btn0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+def buttonClear():
+	userEntry.delete(0,END)
 
-btnAdd = Button(root, text="+", padx=40, pady=20, command=button_add)
-btnEqual = Button(root, text="=", padx=91, pady=20, command=button_add)
-btnClear = Button(root, text="CLEAR", padx=79, pady=20, command=button_add)
+def buttonAdd():
+	userNum = userEntry.get()
+	global firstNum
+	firstNum = int(userNum)
+	userEntry.delete(0,END)
+
+def buttonEqual():
+	secondNum = userEntry.get()
+	userEntry.delete(0,END)
+	userEntry.insert(0, firstNum + int(secondNum))
+
+#Add Button widgets
+btn1 = Button(root, text="1", padx=40, pady=20, command=lambda: buttonClick(1))
+btn2 = Button(root, text="2", padx=40, pady=20, command=lambda: buttonClick(2))
+btn3 = Button(root, text="3", padx=40, pady=20, command=lambda: buttonClick(3))
+btn4 = Button(root, text="4", padx=40, pady=20, command=lambda: buttonClick(4))
+btn5 = Button(root, text="5", padx=40, pady=20, command=lambda: buttonClick(5))
+btn6 = Button(root, text="6", padx=40, pady=20, command=lambda: buttonClick(6))
+btn7 = Button(root, text="7", padx=40, pady=20, command=lambda: buttonClick(7))
+btn8 = Button(root, text="8", padx=40, pady=20, command=lambda: buttonClick(8))
+btn9 = Button(root, text="9", padx=40, pady=20, command=lambda: buttonClick(9))
+btn0 = Button(root, text="0", padx=40, pady=20, command=lambda: buttonClick(0))
+
+btnAdd = Button(root, text="+", padx=40, pady=20, command=buttonAdd)
+btnEqual = Button(root, text="=", padx=99, pady=20, command=buttonClick)
+btnClear = Button(root, text="CLEAR", padx=83, pady=20, command=buttonClear)
 
 
 
